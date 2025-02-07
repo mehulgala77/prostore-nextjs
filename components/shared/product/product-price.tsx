101655d277ec4f8ee2c40fe9ad6163675c82ed56
Mehul Gala <mehulgala007@gmail.com>
2025-02-08 00:51:11 +0530

    Add Product Price

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  // Ensure two decimal places
+  const stringValue = value.toFixed(2);
+
+  // Get the int/float
+  const [intValue, floatValue] = stringValue.split(".");
+
+  return (
+    // Note: 'cn' is used to merge multiple classnames.
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">$</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
