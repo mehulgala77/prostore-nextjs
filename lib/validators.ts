c30c081630e2be22e8cbd76f6ca73a8ea7d6e686
Mehul Gala <mehulgala007@gmail.com>
2025-02-10 19:18:29 +0530

    Added Cart Zod Schema and Prisma Model

--- lib/validators.ts
+++ lib/validators.ts
@@ -45,3 +45,24 @@ export const signUpFormSchema = z
     // Note: Field on which this error should be shown.
     path: ['confirmPassword'],
   });
+
+// Cart Schemas
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, 'Product is required'),
+  name: z.string().min(1, 'Name is required'),
+  slug: z.string().min(1, 'Slug is required'),
+  qty: z.number().int().nonnegative('Quantity must be a positive number'),
+  image: z.string().min(1, 'Image is required'),
+  price: currency,
+});
+
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemsPrice: currency,
+  totalPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  sessionCartId: z.string().min(1, 'Session cart id is required'),
+  // Note: This is optional because we allow guest users to add items in their cart.
+  userId: z.string().optional().nullable(),
+});
