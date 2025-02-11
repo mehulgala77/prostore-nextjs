661cbc82fbe7300cad006741e25d7ed3af253d7b
Mehul Gala <mehulgala007@gmail.com>
2025-02-11 13:35:00 +0530

    Cart Page and Cart Table

--- lib/utils.ts
+++ lib/utils.ts
@@ -49,10 +49,29 @@ export function formatError(error: any): string {
 // Round number to 2 decimal places
 export function round2(value: number | string) {
   if (typeof value === 'number') {
+    // Note: Number.EPSILON is used to make sure decimal points are calculated with precision.
     return Math.round((value + Number.EPSILON) * 100) / 100;
   } else if (typeof value === 'string') {
     return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
   } else {
     throw new Error('Value is not a number or string');
+  }
+}
+
+// Note: Currency Formatter
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+
+// Format currency using the formatter above
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === "number") {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === "string") {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return "NaN";
   }
 }
