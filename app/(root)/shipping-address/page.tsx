206377351536472e70192142b3dce0e711ff4e89
Mehul Gala <mehulgala007@gmail.com>
2025-02-11 19:40:32 +0530

    Shipping Address Page, Form and Update User Address Action

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -4,6 +4,7 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
+import ShippingAddressForm from "./shipping-address-form";
 
 export const metadata: Metadata = {
   title: "Shipping Address",
@@ -11,18 +12,20 @@ export const metadata: Metadata = {
 
 const ShippingAddressPage = async () => {
   const cart = await getMyCart();
-
   if (!cart || cart.items.length === 0) redirect("/cart");
 
   const session = await auth();
 
   const userId = session?.user?.id;
-
   if (!userId) throw new Error("No user ID");
 
   const user = await getUserById(userId);
 
-  return <>Address</>;
+  return (
+    <>
+      <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
+  );
 };
 
 export default ShippingAddressPage;
