956fa06fbd2975e2135dc07f83cc698471d17719
Mehul Gala <mehulgala007@gmail.com>
2025-03-24 17:59:28 +0530

    Added Search Page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -39,11 +39,18 @@ export async function getAllProducts({
   query,
   limit = PAGE_SIZE,
   page,
+  category,
+  price,
+  rating,
+  sort,
 }: {
   query: string;
   limit?: number;
   page: number;
   category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
 }) {
   // Query filter
   const queryFilter: Prisma.ProductWhereInput =
