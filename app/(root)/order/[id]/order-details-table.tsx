099405fbe09f861792f02617dbc89e3aad3e71bb
Mehul Gala <mehulgala007@gmail.com>
2025-03-28 15:34:21 +0530

    Order form payment intent

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -28,15 +28,18 @@ import {
   updateOrderToPaidCOD,
   deliverOrder,
 } from "@/lib/actions/order.actions";
+import StripePayment from "./stripe-payment";
 
 const OrderDetailsTable = ({
   order,
   paypalClientId,
   isAdmin,
+  stripeClientSecret,
 }: {
   order: Order;
   paypalClientId: string;
   isAdmin: boolean;
+  stripeClientSecret: string | null;
 }) => {
   const {
     id,
@@ -244,6 +247,15 @@ const OrderDetailsTable = ({
                     />
                   </PayPalScriptProvider>
                 </div>
+              )}
+
+              {/* Stripe Payment */}
+              {!isPaid && paymentMethod === "Stripe" && stripeClientSecret && (
+                <StripePayment
+                  priceInCents={Number(order.totalPrice) * 100}
+                  orderId={order.id}
+                  clientSecret={stripeClientSecret}
+                />
               )}
 
               {/* Cash On Delivery */}
--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
 import { auth } from "@/auth";
+import Stripe from "stripe";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -21,6 +22,23 @@ const OrderDetailsPage = async (props: {
 
   const session = await auth();
 
+  let client_secret = null;
+
+  // Check if is not paid and using stripe
+  if (order.paymentMethod === "Stripe" && !order.isPaid) {
+    // Note: Init stripe instance
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+    // Note: Create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: "USD",
+      metadata: { orderId: order.id },
+    });
+
+    client_secret = paymentIntent.client_secret;
+  }
+
   return (
     <OrderDetailsTable
       order={{
@@ -30,6 +48,7 @@ const OrderDetailsPage = async (props: {
       // Note: Passing client id like this because OrderDetailsTable is a client component.
       // And we don't want to access process.env in the client component.
       paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+      stripeClientSecret={client_secret}
       isAdmin={session?.user?.role === "admin" || false}
     />
   );
